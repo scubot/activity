1661ec484a85ad709f0f13f6c05d3f6ae4b08ed0
Anson <git@anson-cheung.com>
2019-04-24 01:02:13 +0100

    Add progress bars

--- activity.py
+++ activity.py
@@ -1,6 +1,10 @@
+import os
+import time
+
 import discord
 from discord.ext import commands
 from tinydb import Query, TinyDB
+import tqdm
 
 
 class Activity(commands.Cog):
@@ -21,11 +25,13 @@ class Activity(commands.Cog):
             except discord.NotFound:
                 continue
 
-    async def sync_channel(self, channel):
+    async def sync_channel(self, channel, pbar, pbar_message):
         channel_id = str(channel.id)
         channel_table = self.db.table(channel_id)
         last_scraped_message = await self.determine_last_message(channel, channel_table)
+        time_last = time.time()
         async for message in channel.history(limit=None, after=last_scraped_message, oldest_first=True):
+            time_now = time.time()
             print(message.content)
             channel_table.insert({
                 'id': message.id,
@@ -33,6 +39,26 @@ class Activity(commands.Cog):
                 'author_id': message.author.id,
                 'content': message.content
             })
+            pbar.update(1)
+            if time_now - time_last >= 5:
+                await pbar_message.edit(content=pbar)
+                time_last = time.time()
+
+    # @commands.Cog.listener()
+    # async def on_message(self, message):
+    #     target = Query()
+    #     if self.blacklist.get(target.id == str(message.channel.id)):
+    #         return
+    #     self.db.table(str(message.channel.id)).insert({
+    #         'id': message.id,
+    #         'timestamp': message.created_at.timestamp(),
+    #         'author_id': message.author.id,
+    #         'content': message.content
+    #     })
+    #
+    # @commands.Cog.listener()
+    # async def on_ready(self):
+    #     print("[INFO] Now scraping missed messages for defined servers")
 
     @commands.group(invoke_without_commands=False)
     async def activity(self, ctx):
@@ -50,14 +76,17 @@ class Activity(commands.Cog):
         for c in update_list:
             if self.blacklist.get(target.id == c.id) is not None:
                 continue
-            await self.sync_channel(c)
+            await ctx.send("Now scraping " + str(c))
+            bar = tqdm.tqdm(total=0, unit=' messages', mininterval=3, file=open(os.devnull, 'w'))
+            m = await ctx.send(bar)
+            await self.sync_channel(c, bar, m)
 
     @commands.has_any_role('moderators', 'admin', 'devs')
     @activity.command(name="blacklist")
     async def blacklist(self, ctx, *, channel: discord.TextChannel):
         target = Query()
         if self.blacklist.get(target.id == channel.id) is None:
-            self.blacklist.insert({'id': channel.id})
+            self.blacklist.insert({'id': str(channel.id)})
 
     @activity.command(name="channel")
     async def channel_stat(self, ctx):
