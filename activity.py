0895bfa2657fa3d05955b0cdaa58ba70edbb9201
Anson <git@anson-cheung.com>
2021-05-04 01:30:51 +0800

    v1.0.0 (#1)
    * Add feature to scrape a server
    
    * Add user ignorelist
    
    * Add support for storing attachment as URLs
    
    * Add storage of guild ID
    
    * Add channel ignorelist

---
+++ activity.py
@@ -0,0 +1,82 @@
+import os
+import time
+
+import discord
+from discord.ext import commands
+import modules.activity.database as database
+import tqdm
+
+
+class Activity(commands.Cog):
+    def __init__(self, bot):
+        self.version = "1.0.0"
+        self.bot = bot
+        self.dao = database.Database('./modules/databases/activity.db')
+        self.dao.setup()
+
+    async def determine_last_message(self, channel):
+        for item in self.dao.get_last_messages(channel):
+            try:
+                i = await channel.fetch_message(item[0])
+                return i
+            except discord.NotFound:
+                continue
+
+    async def sync_channel(self, channel, pbar, pbar_message):
+        last_scraped_message = await self.determine_last_message(channel)
+        time_last = time.time()
+        buffer = []
+        async for message in channel.history(limit=None, after=last_scraped_message, oldest_first=True):
+            buffer.append(message)
+            if len(buffer) >= 10000:
+                self.dao.buffered_message_insert(buffer)
+                buffer = []
+            time_now = time.time()
+            pbar.update(1)
+            if time_now - time_last >= 5:
+                await pbar_message.edit(content=pbar)
+                time_last = time.time()
+
+        self.dao.buffered_message_insert(buffer)
+        await pbar_message.edit(content=pbar)
+        pbar.close()
+
+    @commands.group(invoke_without_commands=False)
+    async def activity(self, ctx):
+        pass
+
+    @activity.command(name="update")
+    async def update(self, ctx, channel: discord.TextChannel = None):
+        update_list = []
+        if not channel:
+            update_list = ctx.guild.text_channels
+        else:
+            update_list.append(channel)
+
+        for c in update_list:
+            if self.dao.is_in_blacklist_channel(c):
+                continue
+            now_scraping = await ctx.send("Now scraping " + c.mention + "...")
+            bar = tqdm.tqdm(total=0, unit=' messages', mininterval=3, file=open(os.devnull, 'w'))
+            progress_bar = await ctx.send(bar)
+            await self.sync_channel(c, bar, progress_bar)
+            await now_scraping.edit(content="Now scraping " + c.mention + "... done.")
+        await ctx.send("[:ok_hand:] Update complete.")
+
+    @commands.has_any_role('Moderators', 'Admin', 'devs')
+    @activity.command(name="ignorechannel")
+    async def blacklist(self, ctx, *, channel: discord.TextChannel):
+        if self.dao.is_in_blacklist_channel(channel):
+            self.dao.remove_blacklist_channel(channel)
+            await ctx.send("[:ok_hand:] Channel has been removed from the blacklist.")
+        else:
+            self.dao.insert_blacklist_channel(channel)
+            await ctx.send("[:ok_hand:] Channel is blacklisted and relevant messages removed.")
+
+
+def setup(bot):
+    bot.add_cog(Activity(bot))
+
+
+def teardown(bot):
+    bot.get_cog("Activity").dao.disconnect()
---
+++ database.py
@@ -0,0 +1,97 @@
+# Database Abstraction Object for storing messages
+
+# Database in use is SQLite3
+import sqlite3
+import discord
+
+
+class Database:
+    def __init__(self, path):
+        self.database = sqlite3.connect(path)
+
+    def disconnect(self):
+        self.database.close()
+
+    # Set-up the database for use or something.
+    def setup(self):
+        c = self.database.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS Blacklist_Channel
+        (channel_id INT UNIQUE NOT NULL)''')
+
+        c.execute('''CREATE TABLE IF NOT EXISTS Blacklist_User
+        (user_id INT UNIQUE NOT NULL)''')
+
+        c.execute('''CREATE TABLE IF NOT EXISTS Messages (
+        guild_id INT NOT NULL, channel_id INT NOT NULL, message_id INT NOT NULL PRIMARY KEY, timestamp INT NOT NULL,
+        author_id INT NOT NULL, content TEXT)''')
+
+        c.execute('''CREATE TABLE IF NOT EXISTS Attachments (message_id INT NOT NULL, file_url TEXT DEFAULT NULL, 
+        FOREIGN KEY (message_id) REFERENCES Messages (message_id))''')
+
+        self.database.commit()
+
+    def is_in_blacklist_channel(self, channel):
+        c = self.database.cursor()
+        c.execute('''SELECT channel_id FROM Blacklist_Channel WHERE channel_id=?''', (channel.id,))
+        return c.fetchone() is not None
+
+    def is_in_blacklist_user(self, user):
+        c = self.database.cursor()
+        c.execute('''SELECT user_id FROM Blacklist_User WHERE user_id=?''', (user.id,))
+        return c.fetchone() is not None
+
+    def get_last_messages(self, channel):
+        # Return None if query is empty
+        c = self.database.cursor()
+        c.execute('''SELECT message_id FROM Messages WHERE channel_id=? ORDER BY timestamp DESC LIMIT 100''',
+                  (channel.id,))
+        results = c.fetchall()
+        return results
+
+    def insert_blacklist_channel(self, channel):
+        c = self.database.cursor()
+        c.execute('''INSERT OR IGNORE INTO Blacklist_Channel VALUES (?)''', (channel.id,))
+        c.execute('''DELETE FROM Messages WHERE channel_id=?''', (channel.id,))
+        self.database.commit()
+
+    def remove_blacklist_channel(self, channel):
+        c = self.database.cursor()
+        c.execute('''DELETE FROM Blacklist_Channel WHERE channel_id=?''', (channel.id,))
+        self.database.commit()
+
+    def insert_blacklist_user(self, user):
+        c = self.database.cursor()
+        c.execute('''INSERT OR IGNORE INTO Blacklist_User VALUES (?)''', (user.id,))
+        self.database.commit()
+
+    def message_insert(self, message):
+        c = self.database.cursor()
+        c.execute('''INSERT INTO Messages VALUES (?, ?, ?, ?, ?, ?)''', (
+            message.guild.id, message.channel.id, message.id, message.created_at.timestamp(), message.author.id,
+            message.content))
+        self.database.commit()
+
+    def make_attachment_tuple(self, message, attachment):
+        try:
+            return message.id, attachment.url
+        except discord.HTTPException:
+            return None
+
+    def buffered_message_insert(self, messages):
+        c = self.database.cursor()
+        c.executemany('INSERT INTO Messages VALUES (?, ?, ?, ?, ?, ?)', [(message.guild.id, message.channel.id,
+                                                                          message.id, message.created_at.timestamp(),
+                                                                          message.author.id, message.content)
+                                                                         for message in messages])
+
+        c.executemany('INSERT INTO Attachments VALUES (?, ?)', filter(lambda x: x is not None,
+                                                                      [self.make_attachment_tuple(message, attachment)
+                                                                       for message in messages
+                                                                       for attachment in message.attachments]))
+        self.database.commit()
+
+    def get_message(self, message_id):
+        pass
+
+    def get_all_messages_from_channel(self, channel_id):
+        pass
